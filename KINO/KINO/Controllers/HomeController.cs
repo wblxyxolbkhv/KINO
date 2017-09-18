a278ce9624816c1d12940f9287cdcca50d71642c
wblxyxolbkhv <wblxyxolb.khv@mail.ru>
2017-09-19 09:21:02 +1000

    начальная верстка

--- KINO/KINO/Controllers/HomeController.cs
+++ KINO/KINO/Controllers/HomeController.cs
@@ -15,14 +15,18 @@ namespace KINO.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Halls()
+        {
 
             return View();
         }
---
+++ KINO/KINO/Scripts/site.js
@@ -0,0 +1,10 @@
+﻿function setMenuItemActive() {
+    jQuery("li>a").each(function () {
+        console.log(window.location.pathname);
+        console.log(jQuery(this).attr("href"));
+        if (window.location.pathname == jQuery(this).attr("href")) {
+            jQuery(this).addClass("menu-item-active");
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", setMenuItemActive);
