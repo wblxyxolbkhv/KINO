2689ba81dcf088a0ceb5a4263b3dbc579c075f32
wblxyxolbkhv <wblxyxolb.khv@mail.ru>
2017-09-26 08:17:15 +1000

    немного верстки

--- KINO/KINO/Controllers/HomeController.cs
+++ KINO/KINO/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KINO.Models;
 
 namespace KINO.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
@@ -27,6 +29,8 @@ namespace KINO.Controllers
 
         public ActionResult Halls()
         {
+            IEnumerable<Hall> halls = db.Halls;
+            ViewBag.Halls = halls;
 
             return View();
         }
---
+++ KINO/KINO/Scripts/slideshow.js
@@ -0,0 +1,28 @@
+﻿var slideIndex = -1;
+var slidesCount = 0;
+function onSlideshowInit() {
+    $(".slide").each(function (index) {
+        slidesCount++;
+    });
+}
+document.addEventListener("DOMContentLoaded", onSlideshowInit);
+function onSlideshowTick() {
+    if (slideIndex + 1 >= slidesCount) {
+        slideIndex = 0;
+    }
+    else {
+        slideIndex++;
+    }
+    console.log(slideIndex)
+    $(".slide").each(function (index) {
+        if (index == slideIndex) {
+            $(this).css("display", "block");
+        }
+        else {
+            $(this).css("display", "none");
+        }
+    });
+    
+}
+document.addEventListener("DOMContentLoaded", onSlideshowTick);
+//setInterval(onSlideshowTick, 3000);
